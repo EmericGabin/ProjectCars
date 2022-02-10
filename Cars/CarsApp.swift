29baec56051ffa16e6e2538d1c8c2e51a8b0442b
emeric <60581105+Strange2g@users.noreply.github.com>
2022-02-10 11:25:49 +0100

    Initial Commit

---
+++ Cars/CarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsApp.swift
+//  Cars
+//
+//  Created by emeric on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
